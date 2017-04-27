54389195aa41ba3c46c1f29814df7ba70a9681ef
Zandria Johnson <zj5ff@mail.missouri.edu>
2017-04-27 14:43:41 -0500

    Update health-report.js

--- ghdata/static/scripts/health-report.js
+++ ghdata/static/scripts/health-report.js
@@ -110,13 +110,13 @@ GHDataReport.prototype.buildReport = function () {
 	this.api.dist_work().then(function (dist_work) {
 	   MG.data_graphic({
     	  title: "Commits/Project",
-    	  data:  MG.convert.date(dist_work, 'date', '%Y-%m-%dT%H:%M:%S.%LZ'),
-    	  chart_type: 'line',
+    	  data: dist_work,
+    	  chart_type: 'point',
     	  least_squares: true,
     	  full_width: true,
            height: 300,
     	  color_range: ['#aaa'],
-    	  x_accessor: 'date',
+    	  x_accessor: 'project_name',
     	  y_accessor: 'numcommits',
     	  target: '#distribution-over-time'
      });
