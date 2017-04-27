cb156f20b40c3ec30caacba9848525d78ed02a73
Zandria Johnson <zj5ff@mail.missouri.edu>
2017-04-27 16:31:44 -0500

    Update ghtorrent.py

--- ghdata/ghtorrent.py
+++ ghdata/ghtorrent.py
@@ -294,12 +294,7 @@ class GHTorrent(object):
     # Zandria's metrics dist_work and reopened_issues
     def dist_work(self, repoid):
         distWorkSQL = s.sql.text("""
-        SELECT projects.name as "project_name", count(commits.id) as "numcommits", date(commits.created_at) as "date"
-        FROM commits
-        JOIN project_commits on commits.id = project_commits.project_id
-        JOIN projects on projects.id = project_commits.project_id
-    	JOIN users on commits.author_id = users.id
-    	GROUP BY MONTH(date)
+     
         """)
 
         return pd.read_sql(distWorkSQL, self.db, params={"repoid": str(repoid)})
