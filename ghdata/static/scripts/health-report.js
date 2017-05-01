371b858bf59aba8fe24703bde85e019fb0e1b888
Zandria Johnson <zj5ff@mail.missouri.edu>
2017-05-01 15:16:42 -0500

    Update health-report.js

--- ghdata/static/scripts/health-report.js
+++ ghdata/static/scripts/health-report.js
@@ -33,7 +33,7 @@ GHDataReport.prototype.buildReport = function () {
         least_squares: true,
         full_width: true,
         height: 300,
-        color_range: ['blue'],
+        color: 'blue',
         x_accessor: 'date',
         y_accessor: 'commits',
         target: '#commits-over-time'
@@ -139,7 +139,7 @@ GHDataReport.prototype.buildReport = function () {
 	//Community Activity
 	this.api.community_activity().then(function (community_activity) {
 	  MG.data_graphic({
-        title: "Community Activity/Year",
+        title: "Community Activity/Month",
         data: MG.convert.date(community_activity, 'date', '%Y-%m-%dT%H:%M:%S.%LZ'),
         chart_type: 'line',
         least_squares: true,
@@ -172,7 +172,7 @@ GHDataReport.prototype.buildReport = function () {
 	//Contributor Diversity
 	this.api.contributor_diversity().then(function (contributor_diversity) {
 		MG.data_graphic({
-              title: "Contributor Diversity",
+              title: "Contributor Diversity/Project",
               data: contributor_diversity,
               chart_type: 'bar',
               least_squares: true,
@@ -190,7 +190,7 @@ GHDataReport.prototype.buildReport = function () {
 	this.api.transparency().then(function (transparency) {
 	console.log(transparency);
 	MG.data_graphic({
-        title: "Number of comments per issue",
+        title: "Avg. Comments/Issue per Project",
         data: MG.convert.date(transparency, 'date', '%Y-%m-%dT%H:%M:%S.%LZ'),
         chart_type: 'point',
         least_squares: true,
